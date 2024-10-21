96a911faba3940736149bdedee16b1595c820dc3
Phanord Roberto <phanordroberto@Phanords-MacBook-Air.local>
2024-10-21 02:48:44 -0400

    Fix: API

--- app/api/create/user-review/route.ts
+++ app/api/create/user-review/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 import { prisma } from "@/libs/prisma.config";
 import { UserReview } from "@/types/model";
